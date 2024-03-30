9646a3178643f5071008d8a3aa86eff46f69467f
Mqestical <145111762+Mqestical@users.noreply.github.com>
2024-03-30 07:13:49 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,8 @@
+<nav class="menu">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="projects.php">Projects</a></li>
+    <li><a href="authors.php">Authors</a></li>
+    <li><a href="download.php">Download</a></li>
+  </ul>
+</nav>
